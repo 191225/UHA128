d0a78557e06a56a29f1cf02c25543eb64a8cbd70
Nano191225 <93137582+191225@users.noreply.github.com>
2023-04-07 14:07:27 +0900

    Create uha128.js

---
+++ uha128.js
@@ -0,0 +1,48 @@
+import { randomInt } from "./randomInt";
+import { Database, ExtendedDatabase } from "./Database";
+const algorithm = new Database("UHA4");
+
+if (!algorithm.sizeAll) generate();
+
+export function generate() {
+    const texts =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!?'`#$%&*+,-._/(){}<>=^~|@:;";
+    const text = [...texts];
+    for (const v of text) {
+        let algorithmString = "";
+        for (let i = 0; i < 4; i++) {
+            const int = randomInt(1, text.length);
+            algorithmString += text[int - 1];
+        }
+        algorithm.set(v, algorithmString);
+    }
+}
+
+export function uha128(string) {
+    for (let i = 0; i < 128; i++) {
+        string = [...string];
+        for (let v in string) {
+            let STR = [...string[v]];
+            for (let i in STR)
+                STR[i] = algorithm.has(STR[i]) ? algorithm.get(STR[i]) : STR[i];
+                string[v] = STR.join("");
+        }
+        string = string.join("");
+        while (string.length >= 256) {
+            string = [...string];
+            for (let i in string) if (i % 2 === 1) string[i] = "";
+            string = string.filter(v => v !== "").join("");
+        }
+    }
+    let removes = 0;
+    if (string.length > 128) {
+        string = [...string];
+        for (let i = 0; i < string.length - 128; i++) {
+            removes += 2;
+            string[removes] = "";
+        }
+        string = string.filter(v => v !== "").join("");
+    }
+    console.warn(string.length);
+    return string;
+}
